b374675d0692a5055f16450abd683c48298866ed
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-18 02:50:08 +0600

    fixed some error on login and signup page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -63,15 +63,15 @@ const Login = () => {
               <br />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 user-icon mt-2"
+                className="h-5 w-5 user-icon mt-2"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
                 />
               </svg>
@@ -90,15 +90,15 @@ const Login = () => {
               <br />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 user-icon"
+                className="h-5 w-5 user-icon"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
                 />
               </svg>
